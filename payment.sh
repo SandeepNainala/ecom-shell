13185d0bbf1ef84c4de6a7a9ea5de04fff95b1a4
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-25 12:41:51 +0530

    Shell - ecom payment script changes - adding print statements

---
+++ payment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+C="\e[36m"
+N="\e[0m"
+
+echo -e " $Y Install Python3 $N "
+yum install python36 gcc python3-devel -y
+
+echo -e " $Y Add Application user $N "
+useradd roboshop
+
+echo -e " $Y Create Application directory $N "
+rm -rf /app
+mkdir /app
+
+echo -e " $Y Download App content $N "
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+
+echo -e " $Y Unzip App content $N "
+cd /app
+unzip /tmp/payment.zip
+
+echo -e " $Y Install Python dependencies $N "
+pip3.6 install -r requirements.txt
+
+echo -e " $Y Update Config file $N "
+cp /home/centos/ecom-shell/payment.service /etc/systemd/system/payment.service
+
+echo -e " $Y Update Config file $N "
+systemctl daemon-reload
+systemctl enable payment
+systemctl restart payment
