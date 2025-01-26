71c60d2700d85e15c925812b4136a1c90c0c1695
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-26 18:05:30 +0530

    Shell - ecom script if conditions - declared a function in the common script and called it in the script of cart user

--- common.sh
+++ common.sh
@@ -14,24 +14,39 @@ print_head() {
   echo -e "$Y =================== $* =================== $N"
 }
 
+func_status_check() {
+  if [ $1 -eq 0 ]; then
+    echo -e " $G SUCCESS $N "
+  else
+    echo -e " $R FAILURE $N "
+    exit 1
+  fi
+
+}
+
 func_schema_setup(){
   if [ "$schema_setup" == "mongo" ]; then
     print_head "Copy mongoDB repo file"
     cp ${script_name}/mongodb.repo /etc/yum.repos.d/mongodb.repo
+    func_status_check $?
 
     print_head "Install MongoDB repo"
     yum install mongodb-org -y
+    func_status_check $?
 
     print_head "Start MongoDB"
     mongo --host mongodb-dev.devops91.cloud </app/schema/${component}.js
+    func_status_check $?
   fi
   if [ "${schema_setup}" == "mysql" ]; then
 
     print_head " install MySQL "
     yum install mysql -y
+    func_status_check $?
 
     print_head " Load schema "
     mysql -h mysql-dev.devops91.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+    func_status_check $?
   fi
 }
 
@@ -39,28 +54,34 @@ func_app_prereq(){
 
   print_head " Add Application user "
   useradd ${app_user}
+  func_status_check $?
 
   print_head " Create Application directory "
   rm -rf /app
   mkdir /app
+  func_status_check $?
 
   print_head " Download App content "
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  func_status_check $?
 
   print_head " Unzip App content "
   cd /app
   unzip /tmp/${component}.zip
+  func_status_check $?
 }
 
 func_systemd_setup(){
 
   print_head " Copy ${component} systemd files "
   cp ${script_name}/${component}.service /etc/systemd/system/${component}.service
+  func_status_check $?
 
   print_head " start ${component}  systemd files "
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
+  func_status_check $?
 
 }
 
@@ -68,14 +89,17 @@ func_nodejs(){
 
   print_head "Configure nodeJs repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  func_status_check $?
 
   print_head "Install NodeJs"
-  dnf install nodejs -y
+  yum install nodejs -y
+  func_status_check $?
 
   func_app_prereq
 
   print_head "Install node dependencies"
   npm install
+  func_status_check $?
 
   func_schema_setup
   func_systemd_setup
@@ -87,22 +111,14 @@ func_java(){
   print_head  " Install maven repository "
   yum install maven -y
 
-  if [ $? -eq 0 ]; then
-    echo -e " $G SUCCESS $N "
-  else
-    echo -e " $R FAILURE $N "
-  fi
+  func_status_check $?
 
   func_app_prereq
 
   print_head " Install maven dependencies "
   mvn clean package
-  if [ $? -eq 0 ]; then
-      echo -e " $G SUCCESS $N "
-    else
-      echo -e " $R FAILURE $N "
-      exit 1
-  fi
+
+  func_status_check $?
 
   mv target/${component}-1.0.jar ${component}.jar
 
