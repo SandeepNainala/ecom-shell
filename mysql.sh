736f6a6d721d77528764865da3b7e43a9f6efc30
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-27 12:54:36 +0530

    Shell - ecom script logfile for output redirection - declared a function in the common script and called it in the script of rabbitmq redis scripts

--- mysql.sh
+++ mysql.sh
@@ -10,19 +10,24 @@ if [ -z "${mysql_root_password}" ]; then
   exit 1
 fi
 
-echo -e " $Y Disabling MySQL default version $N "
-dnf module disable mysql -y
+print_head "MySQL Installation"
+dnf module disable mysql -y &>>$log_file # Disable the default MySQL module
+func_status_check $?
 
-echo -e " $Y copy MySQL repo file $N "
-cp ${script_name}/mysql.repo /etc/yum.repos.d/mysql.repo
+print_head "Setup MySQL Repository"
+cp ${script_name}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file # Copy the MySQL Repo file
+func_status_check $?
 
-echo -e " $ Y Install MySQL $N "
-dnf install mysql-community-server -y
+print_head "Install MySQL"
+dnf install mysql-community-server -y &>>$log_file # Install MySQL
+func_status_check $?
 
-echo -e " $Y Start MySQL $N "
-systemctl enable mysqld
-systemctl start mysqld
+print_head "Start MySQL"
+systemctl enable mysqld &>>$log_file # Start MySQL
+systemctl restart mysqld &>>$log_file # Start MySQL
+func_status_check $?
 
-echo -e " $Y Get Default Password $N "
-mysql_secure_installation --set-root-pass $mysql_root_password
+print_head "Update MySQL Configuration"
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file # Secure MySQL Installation
+func_status_check $?
 
