66a0bbd8d850617e220322c374dfd509da6ea2dd
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-27 12:10:03 +0530

    Shell - ecom script logfile for output redirection - declared a function in the common script and called it in the script of mongo scripts

--- mongodb.sh
+++ mongodb.sh
@@ -4,11 +4,20 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org -y
+print_head "MongoDB Installation"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$logfile   # Copy the MongoDB Repo file
+func_status_check $?
 
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+print_head "Install MongoDB"
+yum install mongodb-org -y &>>$logfile  # Install MongoDB Server
+func_status_check $?
+
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf $>>$logfile  # Update the MongoDB Listen Address from
+func_status_check $?
+
+print_head "Start MongoDB" # Start MongoDB Service
 systemctl enable mongod
 systemctl restart mongod
+func_status_check $?
 
 
