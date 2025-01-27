b2b2ace689d5430984818d840c703bf4544ba175
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-27 12:06:18 +0530

    Shell - ecom script logfile for output redirection - declared a function in the common script and called it in the script of frontend scripts

--- frontend.sh
+++ frontend.sh
@@ -4,12 +4,32 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl start nginx
+component=frontend
+
+
+print_head "Installing Nginx"
+yum install nginx -y &>>$logfile  # Install Nginx
+func_staus_check $?
+
+print_head "Download Frontend Content"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$logfile # Copy the Nginx RoboShop Configuration
+func_staus_check $?
+
+print_head "Update Nginx Configuration"
+rm -rf /usr/share/nginx/html/*  $>>$logfile   # Remove the default Nginx RoboShop Configuration
+func_staus_check $?
+
+print_head "Start Nginx Service"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$logfile  # Download the Frontend Archive
+func_staus_check $?
+
+print_head "Extract Frontend Archive"
+cd /usr/share/nginx/html &>>$logfile  # Extract the Frontend Archive
+unzip /tmp/${component}.zip &>>$logfile # Extract the Frontend Archive
+func_staus_check $?
+
+print_head "Update Nginx Configuration"
+systemctl enable nginx &>>$logfile  # Start Nginx Service
+systemctl restart nginx   # Restart Nginx Service
+func_staus_check $?
 
