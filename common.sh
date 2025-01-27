707b85ae3c9698f3189014a91dbd2e969a175b41
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-27 11:05:55 +0530

    Shell - ecom script logfile for output redirection - declared a function in the common script and called it in the script of cart user

--- common.sh
+++ common.sh
@@ -9,6 +9,7 @@ N="\e[0m"
 app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
+log_file=/tmp/roboshop.log
 
 print_head() {
   echo -e "$Y =================== $* =================== $N"
@@ -28,25 +29,25 @@ func_status_check() {
 func_schema_setup(){
   if [ "$schema_setup" == "mongo" ]; then
     print_head "Copy mongoDB repo file"
-    cp ${script_name}/mongodb.repo /etc/yum.repos.d/mongodb.repo
+    cp ${script_name}/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
     func_status_check $?
 
     print_head "Install MongoDB repo"
-    yum install mongodb-org -y
+    yum install mongodb-org -y &>>$log_file
     func_status_check $?
 
     print_head "Start MongoDB"
-    mongo --host mongodb-dev.devops91.cloud </app/schema/${component}.js
+    mongo --host mongodb-dev.devops91.cloud </app/schema/${component}.js &>>$log_file
     func_status_check $?
   fi
   if [ "${schema_setup}" == "mysql" ]; then
 
     print_head " install MySQL "
-    yum install mysql -y
+    yum install mysql -y &>>$log_file
     func_status_check $?
 
     print_head " Load schema "
-    mysql -h mysql-dev.devops91.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+    mysql -h mysql-dev.devops91.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql  &>>$log_file
     func_status_check $?
   fi
 }
@@ -54,34 +55,34 @@ func_schema_setup(){
 func_app_prereq(){
 
   print_head " Add Application user "
-  useradd ${app_user} &>/tmp/roboshop.log
+  useradd ${app_user} &>>/tmp/roboshop.log
   func_status_check $?
 
   print_head " Create Application directory "
-  rm -rf /app
-  mkdir /app
+  rm -rf /app &>>$log_file
+  mkdir /app  &>>$log_file
   func_status_check $?
 
   print_head " Download App content "
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
   func_status_check $?
 
   print_head " Unzip App content "
   cd /app
-  unzip /tmp/${component}.zip
+  unzip /tmp/${component}.zip &>>$log_file
   func_status_check $?
 }
 
 func_systemd_setup(){
 
   print_head " Copy ${component} systemd files "
-  cp ${script_name}/${component}.service /etc/systemd/system/${component}.service
+  cp ${script_name}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
   func_status_check $?
 
   print_head " start ${component}  systemd files "
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  systemctl daemon-reload &>>$log_file
+  systemctl enable ${component} &>>$log_file
+  systemctl restart ${component} &>>$log_file
   func_status_check $?
 
 }
@@ -89,17 +90,17 @@ func_systemd_setup(){
 func_nodejs(){
 
   print_head "Configure nodeJs repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
   func_status_check $?
 
   print_head "Install NodeJs"
-  yum install nodejs -y
+  yum install nodejs -y &>>$log_file
   func_status_check $?
 
   func_app_prereq
 
   print_head "Install node dependencies"
-  npm install
+  npm install &>>$log_file
   func_status_check $?
 
   func_schema_setup
@@ -110,18 +111,18 @@ func_nodejs(){
 func_java(){
 
   print_head  " Install maven repository "
-  yum install maven -y >/tmp/roboshop.log
+  yum install maven -y &>>$log_file
 
   func_status_check $?
 
   func_app_prereq
 
   print_head " Install maven dependencies "
-  mvn clean package
+  mvn clean package &>>$log_file
 
   func_status_check $?
 
-  mv target/${component}-1.0.jar ${component}.jar
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
 
   func_schema_setup
   func_systemd_setup
